b2d38c4e85bdc6db62c50523fb15f346f507dfca
Chalo Salvador <chalosalvador@gmail.com>
2020-11-11 19:59:10 -0500

    Update environment.prod.ts

--- functions/src/environments/environment.prod.ts
+++ functions/src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {Environment} from './environment.model';
 
 export const environment: Environment = {
@@ -14,20 +13,3 @@ export const environment: Environment = {
   },
   firebase_admin: '../prodServiceAccount.json',
 };
-=======
-import {Environment} from './environment.model';
-
-export const environment: Environment = {
-  mode: 'prod',
-  production: false,
-  firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
-  },
-  firebase_admin: '../prodServiceAccount.json',
-};
->>>>>>> f1953bd3ba17f4998e56001ef018caf0cb975686
